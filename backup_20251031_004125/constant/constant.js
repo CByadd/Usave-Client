5972038aa6329e9aaabec553a59b61ec0af6d820
Srini <srinnivassh@gmail.com>
2025-11-04 17:43:31 +0530

    gooder

--- backup_20251031_004125/constant/constant.js
+++
@@ -1,7 +0,0 @@
-export const SITE_NAME = "Usave";
-export const SITE_DESCRIPTION = "Your Personal Finance Companion";
-export const LOGO_WHITE_BG = "https://res.cloudinary.com/dvmuf6jfj/image/upload/v1757264115/Usave/USave_Commerical_Logo_bivy2e.jpg";
-
-
-
-export const HERO_IMG="https://res.cloudinary.com/dvmuf6jfj/image/upload/v1757264154/Usave/unsplash_qB0ikuBJtHU_xonh35.jpg"
--- backup_20251031_004125/products/page.js
+++
@@ -1,9 +0,0 @@
-"use client";
-import React from 'react';
-import ProductListingPage from '../components/shared/ProductListingPage';
-
-export const dynamic = 'force-dynamic';
-
-export default function ProductsPage() {
-  return <ProductListingPage />;
-}
